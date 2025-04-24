f9ca2851cfeae6cc0ae99c8b8190474bcf2906ca
aidancarlosmoore <aidancarlosmoore@gmail.com>
2025-04-23 17:47:11 -0700

    Adding Javascript

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,6 +1,28 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Aidan Moore
+// Date: 23 April 2025
+
+// Defining car variables
+let make = "Kia";
+let model = "Rio";
+let color = "Gray";
+let year = "2021";
+let ownIt = true;
+
+// Calculating the difference between the current year and the car's year
+let age = 2025 - year
+
+// Printing all the variables
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years" + "<br>");
+
+
+
+
+
 
 // Constants
 

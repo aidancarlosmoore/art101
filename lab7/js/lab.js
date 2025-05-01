c8eab75067a33d1156c824ed5c68ffde25b8b485
aidancarlosmoore <aidancarlosmoore@gmail.com>
2025-04-30 17:52:37 -0700

    Debugging

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -3,20 +3,32 @@
 // Date: 4/30/25
 
 function getPlayerName() {
-  let userName = window.prompt("Please enter your name", "Spencer Hastings");
+  // This has the player write enter their name and gives a default value in case they put nothing
+  let userName = window.prompt("Please enter your name.", "Spencer Hastings");
   console.log("User's name: ", userName);
 
-  let userNameArray = userName.split("");
+  // TASK X: BONUS! This puts the player's name in lowercase so it can be sorted correctly
+  let userNameCapitalizated = userName.toLowerCase()
+  console.log("User's name without capital letters: ", userName);
+  
+  // This splits the user's name from a string into an array
+  let userNameArray = userNameCapitalized.split("");
   console.log("User's name as an array: ", userNameArray);
-
+  
+  // Now that it's an array, the user's name can be sorted
   let userNameArraySorted = userNameArray.sort();
   console.log("User's name in a sorted array: ", userNameArraySorted);
 
+  // This joins back the sorted array back into a string in alphabetical order
   let userNameSorted = userNameArraySorted.join("");
+  console.log("User's name sorted in string form: ", userNameSorted);
+
+  // This retuns the player's sorted name so it can be displayed
   return userNameSorted;
 
 }
 
+// This calls  the function and displays the final result with a message
 document.writeln("Hey, it seems like this is your name: ",
     getPlayerName(), "</br>");
 

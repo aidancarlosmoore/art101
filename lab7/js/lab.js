e64148a3cb6226231eb3365d4e6abedf150ae343
aidancarlosmoore <aidancarlosmoore@gmail.com>
2025-04-30 17:46:03 -0700

    Slay

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -1,10 +1,26 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Aidan Moore
+// Date: 4/30/25
 
-// Constants
+function getPlayerName() {
+  let userName = window.prompt("Please enter your name", "Spencer Hastings");
+  console.log("User's name: ", userName);
 
-// Functions
+  let userNameArray = userName.split("");
+  console.log("User's name as an array: ", userNameArray);
+
+  let userNameArraySorted = userNameArray.sort();
+  console.log("User's name in a sorted array: ", userNameArraySorted);
+
+  let userNameSorted = userNameArraySorted.join("");
+  return userNameSorted;
+
+}
+
+document.writeln("Hey, it seems like this is your name: ",
+    getPlayerName(), "</br>");
+
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
